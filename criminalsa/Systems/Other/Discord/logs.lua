81b0193a6d221ff63fcc14fdc7aced877d01e7a0
DORTEY <dortey@outlook.de>
2025-03-08 13:33:35 +0100

    .

--- criminalsa/Systems/Other/Discord/logs.lua
+++ criminalsa/Systems/Other/Discord/logs.lua
@@ -1,14 +1,14 @@
 local WEBHOOKS={
-	[1]="https://discord.com/api/webhooks/1249685570410774574/GZ395E29EqxpWcwvKU1nmORzqrPzZlGl05oQlrHJQyMga8XV72wuIz2Q7CueP8IfSLHn",--chat (local, global & /me)
-	[2]="https://discord.com/api/webhooks/1273932825241976965/53CUsgx_Irnz54cznqeHzxqB2jbgW0-ixNzCCgGcagxdpmceI3yp6POOa0fAWgrY9VP9",--registration
-	[3]="https://discord.com/api/webhooks/1307547084680859658/Wl00rfBuAd9AyyaE1aQZtSuMH5xPTJXsGkS0l4_00ayqxh78E5ZMtNZJUKpCcE0a-Mwo",--join/leave
-	[4]="https://discord.com/api/webhooks/1306233187881455708/PilvgmxUyRxh_lPojcelgFywMaOwqk69kgmMdfGP8eiru9mC_LywXUR6eNI58k9LTDlZ",--robbed money deliver
-	[5]="https://discord.com/api/webhooks/1306237220297900083/238XC2zN-9BqSebpmdHckQnT6aPCZTDIUfNJ4zpx0my25mZFTwwMpHoNdrGhFmL4scgt",--trader
-	[6]="https://discord.com/api/webhooks/1306238653420273757/224xd7ay67tPjQd_iGga2Q2po4eylkbb8JjH_5ZrJXRj8K3WhPEFVgsHH7oQXZIsfcyg",--vehicle
-	[7]="https://discord.com/api/webhooks/1273935982370226207/Q2BAEIFuxWPYV4bjixvwEki9IHyntySmuJlMVnJroXBG3svEWQFqLvgLquCcylsotYKe",--hideout
-	[8]="https://discord.com/api/webhooks/1307619266966716416/MFn9QyxiQZLK-1DHvtBTeZ-s4bz05CL_4btTUcLqgguozuZJ00AAWzglpqTdjmIq_fuc",--code redeems
-	[9]="https://discord.com/api/webhooks/1319692659937382400/tka8rurMEKeQoCejQCULEc8YO5krDbTW57y5Ev3oWxtCcZPLOn7Zr9bJrZTwaMTuXS-p",--crates
-	[10]="https://discord.com/api/webhooks/1321569214477242421/9ACoWD7i08NahCvIlu0tfnbaqR-iJ0aZf1qhV6NEK1jQtimk5YiwyiUM1GBBJkEPDo-K",--screensots
+	[1]="",--chat (local, global & /me)
+	[2]="",--registration
+	[3]="",--join/leave
+	[4]="",--robbed money deliver
+	[5]="",--trader
+	[6]="",--vehicle
+	[7]="",--hideout
+	[8]="",--code redeems
+	[9]="",--crates
+	[10]="",--screensots
 };
 
 function sendDiscordMessage(data)
--- criminalsa/Systems/Other/Discord/server.lua
+++ criminalsa/Systems/Other/Discord/server.lua
@@ -6,16 +6,3 @@ addEventHandler("onPlayerResourceStart",root,function(theResource)
         triggerClientEvent(source,"addPlayerRichPresence",source,Server.Discord);
     end
 end)
-
-
---[[ addEventHandler("Logs:Screenshots",root,function()
-    print("alo")
-    takePlayerScreenShot(client,1920,1080,"logs",50,10000,400);
-end)
-
-addEventHandler("onPlayerScreenShot",root,function(resource,status,pixels,timestamp,tag)
-    if(status=="ok")then
-        print("koks")
-        sendDiscordMessage(10,getPlayerName(source).." took a screenshot!",{pixels});
-    end
-end) ]]
